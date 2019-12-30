5a1504dbf4694c0157ae4b032e47af8a4223a632
Vinicius Gularte <viniciusgulartemartin@gmail.com>
2019-12-30 11:41:26 -0300

    pequenas mudanças

--- src/filterListItem.js
+++ src/filterListItem.js
@@ -1,38 +1,47 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
 class FilterListItem extends React.Component {
-	constructor(props){
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this._initMethods();
-		this.state = {};
-	}
+    this._initMethods();
+    this.state = {};
+  }
 
-	/*Bind Functions to Context*/
-	_initMethods(){
-		this._checkBoxClicked = this._checkBoxClicked.bind(this);
-	}
+  /*Bind Functions to Context*/
+  _initMethods() {
+    this._checkBoxClicked = this._checkBoxClicked.bind(this);
+  }
 
-	_checkBoxClicked(){
-		this.props.filterClicked(this.props.index);
-	}
+  _checkBoxClicked() {
+    this.props.filterClicked(this.props.index);
+  }
 
-	render(){
-		const checkBoxClass = [this.props.selected ? "selected " : "", "filter-check-box"].join('');
-		return (<div className="filter-list-item ripple" onClick={this._checkBoxClicked}>
-					<div className={ checkBoxClass } ></div>
-					<div className="filter-label">{this.props.label}</div>
-				</div>);
-	}
+  render() {
+    const checkBoxClass = [
+      this.props.selected ? "selected " : "",
+      "filter-check-box"
+    ].join("");
+    const labelClass = this.props.selected ? "selected-label " : "";
+
+    return (
+      <div
+        className={"filter-list-item ripple" + labelClass}
+        onClick={this._checkBoxClicked}
+      >
+        <div className={checkBoxClass}></div>
+        <div className="filter-label">{this.props.label}</div>
+      </div>
+    );
+  }
 }
-
 
 FilterListItem.propTypes = {
-	filterClicked: PropTypes.func.isRequired,
-	index: PropTypes.number.isRequired,
-	label: PropTypes.any.isRequired,
-	selected: PropTypes.bool.isRequired,
-}
+  filterClicked: PropTypes.func.isRequired,
+  index: PropTypes.number.isRequired,
+  label: PropTypes.any.isRequired,
+  selected: PropTypes.bool.isRequired
+};
 
-export default FilterListItem
+export default FilterListItem;
