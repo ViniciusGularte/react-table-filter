9c81dc8559854d070ee72db4a2e3a8e7a6d44fae
vinicius <viniciusgulartemartin@gmail.com>
2020-01-14 14:16:53 -0300

    fix estilos

--- src/filterIcon.js
+++ src/filterIcon.js
@@ -1,33 +1,35 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
 class FilterIcon extends React.Component {
-	constructor(props){
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this._initMethods();
+    this._initMethods();
 
-		this.state = {};
-	}
+    this.state = {};
+  }
 
-	_initMethods(){
-		this._iconClicked = this._iconClicked.bind(this);
-	}
+  _initMethods() {
+    this._iconClicked = this._iconClicked.bind(this);
+  }
 
-	_iconClicked(){
-		this.props.iconClicked && this.props.iconClicked();
-	}
+  _iconClicked() {
+    this.props.iconClicked && this.props.iconClicked();
+  }
 
-	render(){
-		const className = [this.props.selected ? "selected ": "", "table-filter-icon"].join('');
-		return (<div onClick={ this._iconClicked } className={ className }></div>);
-	}
+  render() {
+    const className = [
+      this.props.selected ? "selected " : "",
+      "fa fa-filter table-filter-icon"
+    ].join("");
+    return <div onClick={this._iconClicked} className={className}></div>;
+  }
 }
-
 
 FilterIcon.propTypes = {
-	iconClicked: PropTypes.func.isRequired, // Function to be called when filter icon is clicked
-	selected: PropTypes.bool,
-}
+  iconClicked: PropTypes.func.isRequired, // Function to be called when filter icon is clicked
+  selected: PropTypes.bool
+};
 
-export default FilterIcon
+export default FilterIcon;
